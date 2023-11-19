b354bfb394d44804bd6e170e7c5a6853c7a1d980
Anay <anaymoitra@gmail.com>
2023-11-19 01:42:51 -0500

    more jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ const WordleGame = () => {
   const [TARGET_WORD, setTargetWord] = useState('');
   const [guess, setGuess] = useState('');
   const [attempts, setAttempts] = useState(0);
-  const [matchedLetters, setMatchedLetters] = useState([]);
-  const [previousGuesses, setPreviousGuesses] = useState([]);
+  const [userPreviousGuesses, setUserPreviousGuesses] = useState([]);
+  const [botPreviousGuesses, setBotPreviousGuesses] = useState([]);
   const [gameOver, setGameOver] = useState(false);
 
   useEffect(() => {
@@ -18,110 +18,135 @@ const WordleGame = () => {
         setWordList(wordsArray.filter(word => word.length === 5));
         const randomIndex = Math.floor(Math.random() * wordsArray.length);
         setTargetWord(wordsArray[randomIndex]);
-        setMatchedLetters(Array(5).fill(null));
       })
       .catch(error => console.error(error));
   }, []);
 
+  // lowercase
   const handleInputChange = (event) => {
     setGuess(event.target.value.trim().toLowerCase().substr(0, 5));
   };
-
+  
   const handleGuess = () => {
     if (!wordList.includes(guess) || guess.length !== 5 || gameOver) return;
 
     const newAttempts = attempts + 1;
     setAttempts(newAttempts);
 
-    const newMatchedLetters = [...matchedLetters];
-    let allLettersMatch = true;
+    const newMatchedLetters = checkMatchedLetters(guess, TARGET_WORD);
 
-    for (let i = 0; i < TARGET_WORD.length; i++) {
-      if (guess[i] === TARGET_WORD[i]) {
+    setUserPreviousGuesses(prevGuesses => [...prevGuesses, { guess, feedback: newMatchedLetters }]);
+
+    if (newMatchedLetters.every(matched => matched === 'green') || newAttempts === 6) {
+      setGameOver(true);
+    } else {
+      setTimeout(handleBotGuess, 1000);
+    }
+  };
+
+  const handleBotGuess = () => {
+    if (gameOver) return;
+
+    const botGuessIndex = Math.floor(Math.random() * wordList.length);
+    const botGuessedWord = wordList[botGuessIndex];
+
+    const newAttempts = attempts + 1;
+    setAttempts(newAttempts);
+
+    const newBotMatchedLetters = checkMatchedLetters(botGuessedWord, TARGET_WORD);
+
+    setBotPreviousGuesses(prevGuesses => [...prevGuesses, { guess: botGuessedWord, feedback: newBotMatchedLetters }]);
+
+    if (newBotMatchedLetters.every(matched => matched === 'green') || newAttempts === 6) {
+      setGameOver(true);
+    }
+  };
+
+  const checkMatchedLetters = (guessWord, targetWord) => {
+    const newMatchedLetters = Array(5).fill(null);
+
+    for (let i = 0; i < targetWord.length; i++) {
+      if (guessWord[i] === targetWord[i]) {
         newMatchedLetters[i] = 'green';
-      } else if (TARGET_WORD.includes(guess[i])) {
+      } else if (targetWord.includes(guessWord[i])) {
         newMatchedLetters[i] = 'yellow';
-        allLettersMatch = false; // To prevent immediate game over if any letter is in the wrong position
-      } else {
-        newMatchedLetters[i] = null;
-        allLettersMatch = false;
       }
     }
 
-    setMatchedLetters(newMatchedLetters);
-
-    setPreviousGuesses(prevGuesses => [...prevGuesses, { guess, feedback: newMatchedLetters }]);
-
-    if (allLettersMatch || newAttempts === 6) {
-      setGameOver(true);
-    }
+    return newMatchedLetters;
   };
 
   const resetGame = () => {
     setGuess('');
     setAttempts(0);
     setGameOver(false);
-    setMatchedLetters(Array(5).fill(null));
-    setPreviousGuesses([]);
+    setUserPreviousGuesses([]);
+    setBotPreviousGuesses([]);
     const randomIndex = Math.floor(Math.random() * wordList.length);
     setTargetWord(wordList[randomIndex]);
   };
 
-  const renderWord = () => {
-    return (
-      <div className="word-display">
-        {TARGET_WORD &&
-          TARGET_WORD.split('').map((letter, index) => {
-            const highlight = matchedLetters[index];
-
-            return (
-              <span key={index} className={`letter ${highlight}`}>
-                {highlight === 'green' || highlight === 'yellow' ? letter.toUpperCase() : '_'}
-              </span>
-            );
-          })}
-      </div>
-    );
-  };
-
   return (
-    <div className="wordle-game">
-      <h1>Wordle Game</h1>
-      <p>Guess the word (5-letter word for this example)</p>
-      {renderWord()}
-      <input
-        type="text"
-        maxLength={5}
-        value={guess}
-        onChange={handleInputChange}
-        disabled={gameOver}
-      />
-      <button onClick={handleGuess} disabled={gameOver}>
-        Guess
-      </button>
-      <button onClick={resetGame}>
+    <div className="wordle-game-container">
+      <div className="player-game">
+        <h1>Wordle</h1>
+        <p>Attempts: {attempts}</p>
+        {userPreviousGuesses.length > 0 && (
+          <div>
+            <h2>Your Previous Guesses:</h2>
+            <ul>
+              {userPreviousGuesses.map((prevGuess, index) => (
+                <li key={index}>
+                  {prevGuess.guess.toUpperCase()} - {prevGuess.feedback.join(', ')}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <input
+          type="text"
+          maxLength={5}
+          value={guess}
+          onChange={handleInputChange}
+          disabled={gameOver}
+        />
+        <button onClick={handleGuess} disabled={gameOver}>
+          Guess
+        </button>
+        {gameOver && (
+          <p className="game-over">
+            {userPreviousGuesses[userPreviousGuesses.length - 1].feedback.every(matched => matched === 'green')
+              ? 'You guessed the word!'
+              : 'You lost!'} The word was: {TARGET_WORD.toUpperCase()}
+          </p>
+        )}
+      </div>
+      <div className="bot-game">
+        <h1>Bot's Game</h1>
+        <p>Attempts: {attempts}</p>
+        {botPreviousGuesses.length > 0 && (
+          <div>
+            <h2>Bot's Previous Guesses:</h2>
+            <ul>
+              {botPreviousGuesses.map((prevGuess, index) => (
+                <li key={index}>
+                  {prevGuess.guess.toUpperCase()} - {prevGuess.feedback.join(', ')}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {gameOver && (
+          <p className="game-over">
+            {botPreviousGuesses[botPreviousGuesses.length - 1].feedback.every(matched => matched === 'green')
+              ? 'Bot guessed the word!'
+              : 'Bot lost!'} The word was: {TARGET_WORD.toUpperCase()}
+          </p>
+        )}
+      </div>
+      <button onClick={resetGame} className="reset-button">
         Reset Game
       </button>
-      <p>Attempts: {attempts}</p>
-      {previousGuesses.length > 0 && (
-        <div>
-          <h2>Previous Guesses:</h2>
-          <ul>
-            {previousGuesses.map((prevGuess, index) => (
-              <li key={index}>
-                {prevGuess.guess.toUpperCase()} - {prevGuess.feedback.join(', ')}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-      {gameOver && (
-        <p className="game-over">
-          {matchedLetters.every(matched => matched === 'green')
-            ? 'You guessed the word!'
-            : 'You lost!'} The word was: {TARGET_WORD.toUpperCase()}
-        </p>
-      )}
     </div>
   );
 };
