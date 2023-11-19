46317d7ce1dcf44a5be258982d4916ad1e00650b
Anay Moitra <145285745+amoitra1@users.noreply.github.com>
2023-11-19 00:37:17 -0500

    Add files via upload

---
+++ wordleGame.jsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import "./wordleGame.css";
+
+const WordleGame = () => {
+  const [wordList, setWordList] = useState([]);
+  const [TARGET_WORD, setTargetWord] = useState('');
+  const [guess, setGuess] = useState('');
+  const [attempts, setAttempts] = useState(0);
+  const [matchedLetters, setMatchedLetters] = useState([]);
+  const [previousGuesses, setPreviousGuesses] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(() => {
+    fetch('/five_letter_words.txt')
+      .then(response => response.text())
+      .then(text => {
+        const wordsArray = text.split('\n').map(word => word.trim().toLowerCase());
+        setWordList(wordsArray.filter(word => word.length === 5));
+        const randomIndex = Math.floor(Math.random() * wordsArray.length);
+
+        //Gets random target word
+        setTargetWord(wordsArray[randomIndex]);
+        setMatchedLetters(Array(5).fill(null));
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  //To lowercase for file comparison
+  const handleInputChange = (event) => {
+    setGuess(event.target.value.trim().toLowerCase().substr(0, 5));
+  };
+
+  //Ensures valid guess
+  const handleGuess = () => {
+  if (!wordList.includes(guess) || guess.length !== 5 || gameOver) return;
+  
+  //Attempt increment
+  const newAttempts = attempts + 1;
+  setAttempts(newAttempts);
+
+  const newMatchedLetters = Array(5).fill(null);
+
+  //Main Logic
+  for (let i = 0; i < TARGET_WORD.length; i++) {
+    if (guess[i] === TARGET_WORD[i]) {
+      newMatchedLetters[i] = 'green';
+    } else if (TARGET_WORD.includes(guess[i]) /*&& TARGET_WORD.indexOf(guess[i]) !== i */ && !newMatchedLetters.includes('yellow')) {
+      newMatchedLetters[i] = 'yellow';
+    } else {
+      newMatchedLetters[i] = 'gray';
+    }
+  }
+
+  setMatchedLetters(newMatchedLetters);
+
+  setPreviousGuesses(prevGuesses => [...prevGuesses, { guess, feedback: newMatchedLetters }]);
+
+  if (newMatchedLetters.every(matched => matched === 'green') || newAttempts === 6) {
+    setGameOver(true);
+  }
+};
+
+
+  const resetGame = () => {
+    setGuess('');
+    setAttempts(0);
+    setGameOver(false);
+    setMatchedLetters(Array(5).fill(null));
+    setPreviousGuesses([]);
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    setTargetWord(wordList[randomIndex]);
+  };
+
+  const renderWord = () => {
+    if (!gameOver) {
+      return null; // Do not render anything if the game is not over yet
+    }
+  
+    return (
+      <div className="word-display">
+        {guess &&
+          guess.split('').map((letter, index) => {
+            const isCorrect = letter === TARGET_WORD[index];
+            const isInWord = TARGET_WORD.includes(letter);
+  
+            let highlight = null;
+            if (isCorrect) {
+              highlight = 'green';
+            } else if (!isCorrect && isInWord) {
+              highlight = 'yellow';
+            } else {
+              highlight = 'gray';
+            }
+  
+            return (
+              <span key={index} className={`letter ${highlight}`}>
+                {highlight !== 'green' ? letter.toUpperCase() : '_'}
+              </span>
+            );
+          })}
+      </div>
+    );
+  };
+  
+  
+
+  return (
+    <div className="wordle-game">
+      <h1>Wordle Game</h1>
+      <p>Guess the word (5-letter word for this example)</p>
+      {renderWord()}
+      <input
+        type="text"
+        maxLength={5}
+        value={guess}
+        onChange={handleInputChange}
+        disabled={gameOver}
+      />
+      <button onClick={handleGuess} disabled={gameOver}>
+        Guess
+      </button>
+      <button onClick={resetGame}>
+        Reset Game
+      </button>
+      <p>Attempts: {attempts}</p>
+      {previousGuesses.length > 0 && (
+        <div>
+          <h2>Previous Guesses:</h2>
+          <ul>
+            {previousGuesses.map((prevGuess, index) => (
+              <li key={index}>
+                {prevGuess.guess.toUpperCase()} - {prevGuess.feedback.join(', ')}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {gameOver && (
+        <p className="game-over">
+          {matchedLetters.every(matched => matched === 'green')
+            ? 'You guessed the word!'
+            : 'You lost!'} The word was: {TARGET_WORD.toUpperCase()}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default WordleGame;
